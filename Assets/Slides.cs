57abc5b0b32fd2e73e1beb51abee39aad3aa8a09
Yuval Kalugny <kalugny@gmail.com>
2014-02-19 15:11:51 +0200

    Init

---
+++ Assets/Slides.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slides : MonoBehaviour {
+	
+	public Texture2D[] TheSlides;
+	public float Delay = 5;
+	
+	private int i = 0;
+	
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = TheSlides[0];
+		StartCoroutine(DisplaySlides());
+	}
+	
+	IEnumerator DisplaySlides(){
+		while (true){
+			yield return new WaitForSeconds(Delay);
+			i++;
+			if (i >= TheSlides.Length) {
+				i = 0;
+			}
+			renderer.material.mainTexture = TheSlides[i];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public Texture2D Hair;
+	public Texture2D Sun;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI(){
+		
+		int SunSize = Screen.height / 10;
+		GUIStyle gs = new GUIStyle();
+		gs.fontSize = SunSize / 2;
+		gs.alignment = TextAnchor.MiddleLeft;
+		
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Hair);
+		GUI.DrawTexture(new Rect(0, Screen.height - SunSize, SunSize, SunSize), Sun);
+		GUI.Label(new Rect(SunSize, Screen.height - SunSize, SunSize, SunSize), "28°C", gs);
+	}
+}
